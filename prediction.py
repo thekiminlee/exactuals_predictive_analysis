ed8a37335d0f668c57e541b377409afd9320b810
Ki Min Lee <thekiminlee@gmail.com>
2020-05-13 22:11:39 -0700

    update model

--- prediction.py
+++ prediction.py
@@ -2,23 +2,16 @@ from xgboost import XGBClassifier
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
+models = ['exactuals_001_label1.model', 'exactuals_001_label2.model', 'exactuals_001_label3.model']
+labels = ['payee_satisfaction', 'payor_satisfaction', 'overall_satisfaction']
+
 # loading model
-model = XGBClassifier()
-model.load_model('exactuals_001.model')
-
-# accepting user data
 df = pd.read_csv('test.csv')
+df = df.drop(columns=["Unnamed: 0", "transaction_start_date", "transaction_end_date"]+labels, axis=1)
 
-df = df.drop(columns=['payee_rating'], axis=1)
-df = df.drop(columns=['Unnamed: 0'], axis=1)
+for i in range(0, 3):
+    model = XGBClassifier()
+    model.load_model(models[i])
+    prediction = model.predict(df)
+    print(labels[i], prediction)
 
-le = LabelEncoder()
-for attr in df:
-    if(df[attr].dtype == 'object'):
-        df.loc[:,(attr)] = le.fit_transform(df.loc[:,(attr)])
-
-
-print(df)
-
-prediction = model.predict(df)
-print(prediction)
--- train.py
+++ train.py
@@ -7,37 +7,53 @@ from xgboost import XGBClassifier
 
 
 class Exactuals():
-    def __init__(self, _file, _label):
+    def __init__(self, _file, _label1, _label2, _label3):
         self.df = pd.read_csv(_file)
-        self.df = self.df.drop(columns=["Unnamed: 0"], axis=1)
-
+        self.df = self.df.drop(columns=["Unnamed: 0", "transaction_start_date", "transaction_end_date"], axis=1)
+    
         le = LabelEncoder()
         for attr in self.df:
             if(self.df[attr].dtype == 'object'):
                 self.df.loc[:,(attr)] = le.fit_transform(self.df.loc[:,(attr)])
 
-        self.feature = self.df.drop(columns=[_label], axis=1)
-        self.label = self.df[_label]
+        self.feature = self.df.drop(columns=[_label1, _label2, _label3], axis=1)
+        self.label1 = self.df[_label1]
+        self.label2 = self.df[_label2]
+        self.label3 = self.df[_label3]
+        print(self.feature.head())
+        # label 1
+        self.label1_set = train_test_split(self.feature, self.label1, test_size=0.3)
+        # label 2
+        self.label2_set = train_test_split(self.feature, self.label2, test_size=0.3)
+        # label 3
+        self.label3_set = train_test_split(self.feature, self.label3, test_size=0.3)
 
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.feature, self.label, test_size=0.3)
-        
         self.prediction = None
         self.model = XGBClassifier()
 
-    def train(self):
+    def train(self, label_set):
         print("Training XGB model...")
-        self.model.fit(self.X_train, self.y_train)
+        self.model.fit(label_set[0], label_set[2])
+        pred = self.model.predict(label_set[1])
+        print(pred, label_set[1])
         print("Training complete")
 
-    def evaluate(self):
-        cvs = cross_val_score(estimator=self.model, X=self.X_train, y=self.y_train, cv=10, n_jobs=-1)
+    def evaluate(self, label_set):
+        cvs = cross_val_score(estimator=self.model, X=label_set[0], y=label_set[2], cv=10, n_jobs=-1)
         print("Accuracy: ", round(cvs.mean()*100, 2), "%")
 
     def export(self, name):
         self.model.save_model(name)
 
+    def start(self,start):
+        count = 1
+        labels = [self.label1_set, self.label2_set, self.label3_set]
+        for label in labels:
+            self.train(label)
+            self.evaluate(label)
+            self.export('exactuals_'+ start + "_label" + str(count) + ".model")
+            count += 1
+
 if __name__ == '__main__':
-    exactuals = Exactuals('sample.csv', 'payee_rating')
-    exactuals.train()
-    exactuals.evaluate()
-    exactuals.export('exactuals_001.model')
+    exactuals = Exactuals('sample.csv', 'payee_satisfaction','payor_satisfaction', 'overall_satisfaction')
+    exactuals.start("001")
