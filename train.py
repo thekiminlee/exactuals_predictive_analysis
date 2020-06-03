e0a4746ae928730fabc8f4867875f9b9f47466b4
Ki Min Lee <kimin@Kis-MacBook-Pro.lan>
2020-06-03 00:29:49 -0700

    update data set

--- train.py
+++ train.py
@@ -20,7 +20,6 @@ class Exactuals():
         self.label1 = self.df[_label1]
         self.label2 = self.df[_label2]
         self.label3 = self.df[_label3]
-        print(self.feature.head())
         # label 1
         self.label1_set = train_test_split(self.feature, self.label1, test_size=0.3)
         # label 2
@@ -35,7 +34,6 @@ class Exactuals():
         print("Training XGB model...")
         self.model.fit(label_set[0], label_set[2])
         pred = self.model.predict(label_set[1])
-        print(pred, label_set[1])
         print("Training complete")
 
     def evaluate(self, label_set):
@@ -55,5 +53,5 @@ class Exactuals():
             count += 1
 
 if __name__ == '__main__':
-    exactuals = Exactuals('sample.csv', 'payee_satisfaction','payor_satisfaction', 'overall_satisfaction')
-    exactuals.start("001")
+    exactuals = Exactuals('new_data.csv', 'payee_satisfaction','payor_satisfaction', 'overall_satisfaction')
+    exactuals.start("002")
